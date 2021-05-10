893997c14af9e0359fbdde5d2c3e4e140c26a513
andand98 <andand9854@gmail.com>
2021-05-10 15:05:26 +0300

    variables.py

---
+++ FirstDay/variables.py
@@ -0,0 +1,23 @@
+# name = input("What is your name? \n")
+# print(name)
+
+# name = input("What is your name? \n")
+# length = len(name)
+# print(length)
+
+a = input("a: ")
+b = input("b: ")
+
+print("before switch: ")
+print("a = " + a)
+print("b = " + b)
+
+c = a
+a = b
+b = c
+
+print("after switch")
+print("a = " + a)
+print("b = " + b)
+
+
